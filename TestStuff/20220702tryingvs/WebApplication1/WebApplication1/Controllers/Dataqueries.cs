df2db4b10f70ba75cf668702ef3a22312364eecf
Woody <phy5m2w@gmail.com>
2022-07-03 14:16:43 +0100

    whatever changes woody made while we were figuring things out

--- TestStuff/20220702tryingvs/WebApplication1/WebApplication1/Controllers/Dataqueries.cs
+++ TestStuff/20220702tryingvs/WebApplication1/WebApplication1/Controllers/Dataqueries.cs
@@ -19,7 +19,20 @@ var sql = "SELECT version()";
 
 using var cmd = new NpgsqlCommand(sql, con);
 
+
+
 var version = cmd.ExecuteScalar().ToString();
+
+sql="INSERT INTO \"whatWillMultipleSchemasHelpUsWith\".test2(anything) VALUES (99)";
+sql="select*from \"whatWillMultipleSchemasHelpUsWith\".test2 limit 1";
+
+using var cmd2 = new NpgsqlCommand(sql, con);
+
+Console.WriteLine($"PostgreSQL version: {version}");
+
+//version=cmd2.ExecuteReader();
+
+
 //using new context(){}
 Console.WriteLine($"PostgreSQL version: {version}");
 
--- TestStuff/20220702tryingvs/WebApplication1/WebApplication1/Program.cs
+++ TestStuff/20220702tryingvs/WebApplication1/WebApplication1/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore; // place this line at the beginning of file.
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Connect to PostgreSQL Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<NoteDb>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -25,3 +35,21 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+class NoteDb: DbContext {
+    public NoteDb(DbContextOptions<NoteDb> options): base(options) {
+
+    }
+    public DbSet<Note> Notes => Set<Note>();
+}
+
+
+
+
+
+
+
+record Note(int id){
+    public string text {get;set;} = default!;
+    public bool done {get;set;} = default!;
+}
