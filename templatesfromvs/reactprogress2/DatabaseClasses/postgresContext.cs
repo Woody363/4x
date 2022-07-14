d8a8b1f275a62750436280e24d441ad8e72e7f77
phil <philip.r.tate.media@gmail.com>
2022-07-14 14:57:38 +0100

    post merge

--- templatesfromvs/reactprogress2/DatabaseClasses/postgresContext.cs
+++ templatesfromvs/reactprogress2/DatabaseClasses/postgresContext.cs
@@ -40,7 +40,7 @@ namespace reactprogress2
             {
                 //qqqq do not commit this
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=qqqqq;Database=postgres;Username=qqqqqq;Password=qqqqq");
+                optionsBuilder.UseNpgsql("Host=qqqq;Database=postgres;Username=qqqq;Password=qqqq");
             }
         }
 
